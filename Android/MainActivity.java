449ddace77cb4552f0c42136b23e877ed2b411b3
Shubham-tiwari123 <st540362@gmail.com>
2019-02-23 12:04:59 +0530

    Android files

--- Android/MainActivity.java
+++ Android/MainActivity.java
@@ -17,9 +17,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     TextView block4;
     TextView block5;
     TextView block6;
-    TextView block7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         block4 = (TextView)findViewById(R.id.block4);
         block5 = (TextView)findViewById(R.id.block5);
         block6 = (TextView)findViewById(R.id.block6);
-        block7 = (TextView)findViewById(R.id.block7);
 
         block1.setVisibility(View.INVISIBLE);
         block2.setVisibility(View.INVISIBLE);
@@ -51,17 +53,30 @@ public class MainActivity extends AppCompatActivity {
         block4.setVisibility(View.INVISIBLE);
         block5.setVisibility(View.INVISIBLE);
         block6.setVisibility(View.INVISIBLE);
-        block7.setVisibility(View.INVISIBLE);
 
-        Button button = (Button)findViewById(R.id.startMotor);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button startMotor = (Button)findViewById(R.id.startMotor);
+        startMotor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TankStatus tankStatus = new TankStatus();
-                tankStatus.execute();
+
             }
         });
 
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask callDisplayOrders = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        TankStatus tankStatus = new TankStatus();
+                        tankStatus.execute();
+                    }
+                });
+            }
+        };
+        timer.schedule(callDisplayOrders,0,3000);
     }
 
     private class TankStatus extends AsyncTask<Void,Void,Void>{
@@ -110,30 +125,106 @@ public class MainActivity extends AppCompatActivity {
             block4 = (TextView)findViewById(R.id.block4);
             block5 = (TextView)findViewById(R.id.block5);
             block6 = (TextView)findViewById(R.id.block6);
-            block7 = (TextView)findViewById(R.id.block7);
 
-            if(distance==345){   /*distance==16*/
-                block7.setVisibility(View.VISIBLE);
-            }
-            else if(distance==12){
+            if(distance>10 && distance<=12){
                 block6.setVisibility(View.VISIBLE);
             }
-            else if(distance==10){
+            else if(distance>8 && distance<=10){
+                block6.setVisibility(View.VISIBLE);
                 block5.setVisibility(View.VISIBLE);
             }
-            else if(distance==8){
+            else if(distance>6 && distance<=8){
+                block6.setVisibility(View.VISIBLE);
+                block5.setVisibility(View.VISIBLE);
                 block4.setVisibility(View.VISIBLE);
             }
-            else if(distance==6){
+            else if(distance>4 && distance<=6){
+                block6.setVisibility(View.VISIBLE);
+                block5.setVisibility(View.VISIBLE);
+                block4.setVisibility(View.VISIBLE);
                 block3.setVisibility(View.VISIBLE);
             }
-            else if(distance==4){
+            else if(distance>=2 &&distance<=4){
+                block6.setVisibility(View.VISIBLE);
+                block5.setVisibility(View.VISIBLE);
+                block4.setVisibility(View.VISIBLE);
+                block3.setVisibility(View.VISIBLE);
                 block2.setVisibility(View.VISIBLE);
-            }
-            else if(distance==2){
                 block1.setVisibility(View.VISIBLE);
             }
+            else if(distance>=-4 &&distance<=-2){
+                block1.setVisibility(View.INVISIBLE);
+            }
+
+            else if(distance>=-6 &&distance<-4){
+                block1.setVisibility(View.INVISIBLE);
+                block2.setVisibility(View.INVISIBLE);
+            }
+            else if(distance>=-8 &&distance<-6){
+                block1.setVisibility(View.INVISIBLE);
+                block2.setVisibility(View.INVISIBLE);
+                block3.setVisibility(View.INVISIBLE);
+            }
+            else if(distance>=-10 &&distance<-8){
+                block1.setVisibility(View.INVISIBLE);
+                block2.setVisibility(View.INVISIBLE);
+                block3.setVisibility(View.INVISIBLE);
+                block4.setVisibility(View.INVISIBLE);
+            }
+            else if(distance>=-12 &&distance<-10){
+                block1.setVisibility(View.INVISIBLE);
+                block2.setVisibility(View.INVISIBLE);
+                block3.setVisibility(View.INVISIBLE);
+                block4.setVisibility(View.INVISIBLE);
+                block5.setVisibility(View.INVISIBLE);
+                block6.setVisibility(View.INVISIBLE);
+            }
             super.onPostExecute(aVoid);
+        }
+    }
+
+    private class UserResponse extends AsyncTask<Void,Void,Void>{
+
+        int flag=0;
+        JSONObject jsonObject = new JSONObject();
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Log.e("trymsg","msg");
+            String URL_POST = "http://192.168.1.198:22849/RaspberryPi/UserResponse";
+            try {
+                jsonObject.put("userResponse","1");
+                URL url = new URL(URL_POST);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.connect();
+                conn.setDoOutput(true);
+                OutputStream outputStream = conn.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(outputStream, "UTF-8");
+                osw.write(jsonObject.toString());
+                osw.flush();
+                osw.close();
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK){
+                    flag=1;
+
+                }
+                Log.e("response", String.valueOf(responseCode));
+            } catch (MalformedURLException e) {
+                Log.e("errors",e.toString());
+            } catch (IOException e) {
+                Log.e("errors",e.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if(flag==1)
+                Toast.makeText(getApplicationContext(),"Motor Stopped",Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(getApplicationContext(),"Some error occurred try again...",Toast.LENGTH_SHORT).show();
         }
     }
 }
