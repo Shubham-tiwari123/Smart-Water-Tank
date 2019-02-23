759431a714bbdbe2c53b30dc57a6c4b32cdf5c27
Shubham-tiwari123 <st540362@gmail.com>
2019-02-23 22:15:31 +0530

    REST API

---
+++ TankStatus.java
@@ -0,0 +1,35 @@
+package RestAPI;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import Database.SaveData;
+import org.json.simple.JSONObject;
+
+public class TankStatus extends HttpServlet {
+    
+    SaveData saveData = new SaveData();
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("hit");
+        int waterLevel = saveData.getData();
+        JSONObject waterObj = new JSONObject();
+        waterObj.put("waterLevel", waterLevel);
+        
+        JSONObject jSONObject = new JSONObject();
+        jSONObject.put("waterObj", waterObj);
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print(jSONObject);
+    }
+}
